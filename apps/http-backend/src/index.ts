9a71b3658cc273a3b0b4ab830f114d4bf57d34fb
Shaan2170 <shaanmustafa1234@gmail.com>
2026-06-24 01:16:02 +0530

    Updated http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -95,6 +95,36 @@ app.post("/room", middleware , async (req, res) => {
         
     }
 })
+    app.get("/chats/:roomId", async (req, res) => {
+    const roomId = Number(req.params.roomId);
+    const messages = await prismaClient.chat.findMany({
+        where: {
+            roomId: roomId
+        },
+        orderBy: {
+            id: "desc"
+        },
+        take: 50
+    });
+
+    res.json({
+        messages
+    })
+})
+
+app.get("/chats/:slug", async (req, res) => {
+    const slug = req.params.slug;
+    const room = await prismaClient.room.findFirst({
+        where: {
+            slug
+        }
+    });
+
+    res.json({
+        room
+    })
+})
+
 
 
 app.listen(3001);
